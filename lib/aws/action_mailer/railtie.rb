3dd8924243c804b3585d72e1f9962ba0aae2eecf
Matt Muller <mamuller@amazon.com>
2024-11-21 13:37:16 -0500

    Fix settings config loading

--- lib/aws/action_mailer/railtie.rb
+++ lib/aws/action_mailer/railtie.rb
@@ -5,11 +5,9 @@ module Aws
     # Automatically configures ActionMailer to support the Amazon SES and SESV2
     # delivery methods.
     class Railtie < Rails::Railtie
-      config.before_initialize do
-        ActiveSupport.on_load(:action_mailer) do
-          add_delivery_method :ses, SES::Mailer
-          add_delivery_method :ses_v2, SESV2::Mailer
-        end
+      ActiveSupport.on_load(:action_mailer) do
+        add_delivery_method :ses, SES::Mailer
+        add_delivery_method :ses_v2, SESV2::Mailer
       end
     end
   end
