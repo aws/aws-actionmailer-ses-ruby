8c144cd0a413e1d16a77c84ceb90552d91110ceb
Juli Tera <terajul@amazon.com>
2026-03-27 12:11:03 -0700

    Add finishing touches for release

--- lib/aws/action_mailer/ses/mailer.rb
+++ lib/aws/action_mailer/ses/mailer.rb
@@ -7,26 +7,33 @@ module Aws
     module SES
       # Provides a delivery method for ActionMailer that uses Amazon Simple Email Service.
       #
-      # Delivery settings are used to construct a new Aws::SES::Client instance.
+      # Delivery settings are used to construct a new `Aws::SES::Client` instance.
       # Once you have a delivery method, you can configure your Rails environment to use it:
       #
       #     config.action_mailer.delivery_method = :ses
       #     config.action_mailer.ses_settings = { region: 'us-west-2' }
       #
+      # Alternatively, you could pass the client itself.
+      #
+      # The passed in client will be prioritized regardless of other `:ses_settings` given.
+      #
       # @see https://guides.rubyonrails.org/action_mailer_basics.html
       class Mailer
         attr_reader :settings
 
-        # @param [Hash] settings Passes along initialization options to
-        #   [Aws::SES::Client.new](https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/SES/Client.html#initialize-instance_method).
-        #   You may pass +:ses_client+ with a preconstructed {Aws::SES::Client} to reuse one
-        #   instance (e.g. to avoid credential refresh churn). That key is not forwarded to the SDK.
+        # @param [Hash] settings
+        #   Passes along initialization options to [Aws::SES::Client.new](https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/SES/Client.html#initialize-instance_method).
+        #   You may pass `:ses_client` with a preconstructed {Aws::SES::Client} to reuse
+        #   an existing instance (e.g. to avoid credential resolution on every delivery).
+        #   When provided, the injected client is used and all other options are ignored.
         def initialize(settings = {})
           @settings = settings
           client_settings = settings.dup
-          injected = client_settings.delete(:ses_client)
-          @client = injected || Aws::SES::Client.new(client_settings)
-          @client.config.user_agent_frameworks << 'aws-actionmailer-ses'
+          @client = client_settings.delete(:ses_client) || Aws::SES::Client.new(client_settings)
+
+          unless @client.config.user_agent_frameworks.include?('aws-actionmailer-ses')
+            @client.config.user_agent_frameworks << 'aws-actionmailer-ses'
+          end
         end
 
         # Delivers a Mail::Message object. Called during mail delivery.
